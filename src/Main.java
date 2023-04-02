b89b77f871d68f08c5fc5932fce433864c1b31c7
Sugrobkin <dsugrobkin148@gmail.com>
2023-04-02 18:11:57 +0300

    code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        int initialbalance = 100;
+        int addend = 1200;
+
+        int bonus = 0;
+        if (addend > 1000) {
+            bonus = addend / 100;
+        }
+
+        int finalBalance = initialbalance + addend + bonus;
+        System.out.printf("Баланс после пополнения: " + finalBalance);
+        System.out.printf(" Начисленные бонусы: "  + bonus);
+
 
     }
 }
